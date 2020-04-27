7c6adba02234e2d08997e94e248f4810d60d2e43
Darren Shepherd <darren@rancher.com>
2020-04-27 16:46:44 -0700

    Fix broken multi-arch script

--- push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker push ibuildthecloud/dapper:test-amd64
-docker push ibuildthecloud/dapper:test-arm
-docker push ibuildthecloud/dapper:test-arm64
-manifest-tool push from-args --platforms linux/arm,linux/arm64,linux/amd64 --template ibuildthecloud/dapper:test-ARCH --target ibuildthecloud/dapper:test
